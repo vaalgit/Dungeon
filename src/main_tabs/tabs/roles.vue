7b1c9b74a9f0f51048c3203f852e914d706ce777
Chih Yuan <chihyuanchou@gmail.com>
2018-04-16 21:54:50 +0800

    add new add-role modal dialog     1. Enter role name in dialog will change tab name is avaliable

--- src/main_tabs/tabs/roles.vue
+++ src/main_tabs/tabs/roles.vue
@@ -3,17 +3,35 @@
 		div.col-3
 			.nav.flex-column.nav-pills.role-sub-tab(role="tablist" aria-orientation="vertical")
 				a.nav-link.active(id="v-pills-home-tab" data-toggle="pill" href="#v-index-tab" role="tab" aria-controls="index-tab" aria-selected="true") first tab
-				a.nav-link(@click="add_content" id="v-pills-home-tab" data-toggle="pill" href="#" role="tab" aria-controls="v-pills-home" aria-selected="false") add
+				a.nav-link.btn.btn-info(@click="add_content" id="v-pills-home-tab" role="tab" data-target="#add_role_modal" data-toggle="modal" type="button") add
 		div.col-9
 			.tab-content.role-sub-content(id="v-pills-tabContent")
 				.tab-pane.fade.show.active(id="v-index-tab" role="tabpanel" aria-labelledby="index-tab-tab") index-tab
-				
-
+		
+		//- button(id="btn_add_role" style="display:none" type="button" data-toggle="modal" data-target="#add_role_modal")
+		//- div.modal.fade(id="add_role_modal" role="dialog")
+		//- 	.modal-dialog
+		//- 		.modal-content
+		//- 			.modal-header
+		//- 				button.close( type="button" data-dismiss="modal") x
+		//- 				h4.modal-title create role
+		//- 			.modal-body
+		//- 				input(id="add_role_data")
+		//- 			.modal-footer
+		//- 				button.btn.btn-default(@click="confirm_add" type="button" data-dismiss="modal") Add
+		//- 				button.btn.btn-default( type="button" data-dismiss="modal") Close
+		add-role
 </template>
 
 <script>
+import Vue from "vue"
+import AddRole from "./utils/addRole.vue";
+
 export default {
   name: "roles-tab",
+  components: {
+		AddRole
+	},
   data() {
     return {
       message: "this is role component"
@@ -22,22 +40,44 @@ export default {
   mounted() {
     console.log("roles.vue mounted.");
   },
-	methods:{
-		add_tab(e){
-			e.preventDefault();
-      if (!$(this).hasClass('add-contact')) {
-          $(this).tab('show');
+  methods: {
+    add_tab(e) {
+      e.preventDefault();
+      if (!$(this).hasClass("add-contact")) {
+        $(this).tab("show");
       }
+    },
+    add_content(e) {
+      // e.preventDefault();
+      var id = $(".nav-pills.role-sub-tab").children().length; //think about it ;)
+			var tabId = "role_" + id;
+			console.log(tabId);
+      $(e.target)
+        .closest("a")
+        .before(
+          '<a class="nav-link" data-toggle="pill" role="tab" aria-controls="v-pills-home" href="#contact_' +
+            id +
+            '">New Tab</a>'
+        );
+      $(".tab-content.role-sub-content").append(
+        '<div class="tab-pane" id="' +
+          tabId +
+          '">Contact Form: New Contact ' +
+          id +
+          "</div>"
+      );
+      $(".nav-pills.role-sub-tab a:nth-child(" + id + ")").click();
+			// $("#add_role_modal").modal("show");
+			var role_tab_target = $(".nav-pills.role-sub-tab a:nth-child(" + id + ")");
+			this.$on('role_tab_name',(name)=>{
+				if (name == '') {
+					name = tabId;
+				}
+				role_tab_target[0].text = name;
+				console.log('triggered role_tab_name event');
+			});
 		},
-		add_content(e){
-			e.preventDefault();
-			var id = $(".nav-pills.role-sub-tab").children().length; //think about it ;)
-			var tabId = 'contact_' + id;
-			$(e.target).closest('a').before('<a class="nav-link" data-toggle="pill" role="tab" aria-controls="v-pills-home" href="#contact_' + id + '">New Tab</a>');
-			$('.tab-content.role-sub-content').append('<div class="tab-pane" id="' + tabId + '">Contact Form: New Contact ' + id + '</div>');
-			$('.nav-pills a:nth-child(' + id + ') a').click();
-		}
-	}
+  }
 };
 </script>
 
---
+++ src/main_tabs/tabs/utils/addRole.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  div.modal.fade(id="add_role_modal" role="dialog")
+    .modal-dialog
+      .modal-content
+        .modal-header
+          h1.modal-title create role
+        .modal-body
+          div.col
+            div.row-2
+              p Enter role name
+                input(v-model="role_name" placeholder="Enter role name")
+            div.row-2.btn-group.btn-group-lg Role: 
+              div Apple
+                button.btn.btn-primary(type="radio" value="Apple" name="radio")
+              div Sony
+                button.btn.btn-primary(type="radio" value="Sony" name="radio")
+              div Samsung
+                button.btn.btn-primary(type="radio" value="Samsung" name="radio")
+
+        .modal-footer
+          button.btn.btn-default(@click="confirm_add" type="button" data-dismiss="modal") Add
+          button.btn.btn-default(type="button" data-dismiss="modal") Close
+</template>
+
+<script>
+export default {
+  name: "add-role",
+  data() {
+    return {
+      message: "this is add role component",
+      role_name:''
+    };
+  },
+  mounted() {
+    console.log("add-roles.vue mounted.");
+  },
+  methods:{
+    confirm_add(){
+      this.$parent.$emit('role_tab_name',this.role_name);
+    }
+  }
+};
+</script>
+
+<style lang="sass">
+
+
+</style>
