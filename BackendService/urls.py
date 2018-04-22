3631e2cd7bddf487a310a9b6d94f97b02d9da784
Gibson <natsumi-0416@hotmail.com>
2018-04-22 10:35:08 +0800

    [dev] add static function in url.py

--- BackendService/urls.py
+++ BackendService/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^$',TemplateView.as_view(template_name="index.html")),
-]
+] + static(settings.STATIC_ROOT, document_root=settings.STATICFILES_DIRS)
